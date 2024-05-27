7d22048b87eeb540f5d98a296fc5ad6b084e438a
root <root@DESKTOP-3I9N5MH>
2024-05-27 15:05:37 +0200

    adding pos_tag_frequency function to the script

--- syntax.py
+++ syntax.py
@@ -1,10 +1,11 @@
 import json
 import spacy
+from collections import defaultdict, Counter
 
 # Load spaCy model
 nlp = spacy.load("en_core_web_sm")
 
-def process_text(file_path):
+def get_content(file_path):
     article_content = []
     with open(file_path, "r", encoding="utf-8") as file:
         for line in file:
@@ -15,11 +16,23 @@ def process_text(file_path):
 
     return article_content
 
+def pos_tag_frequency(article_content):
+    #pos tag frequency 
+    pos_tag_frequency = defaultdict(int)
+    
+    for text in article_content:
+        doc = nlp(text)
+        for token in doc:
+            pos_tag_frequency[token.pos_] += 1
+    
+    counter = Counter(pos_tag_frequency)
+    
+    return counter.most_common()
+
 def main():
     file_path = "human.jsonl"
-    article_content = (process_text(file_path))
-    for text in article_content:
-        print(text)
+    article_content = (get_content(file_path))
+    print(pos_tag_frequency(article_content))
 
 if __name__ == "__main__":
     main()
