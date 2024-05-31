d2e1265910f044b45ad245803353240af1036ee8
KennieSlagter <k.m.slagter@student.rug.nl>
2024-05-31 12:58:37 +0200

    deleting unneccessary code

--- scripts/syntax.py
+++ scripts/syntax.py
@@ -163,54 +163,6 @@ def main():
     human_article_content = (get_content(file_path_human))
     ai_article_content = (get_content(file_path_ai))
 
-    # print('POS Tag frequency - human content:')
-    # print(pos_tag_frequency(human_article_content))
-
-    # print('\nPOS Tag frequency - ai content:')
-    # print(pos_tag_frequency(ai_article_content))
-
-    # human_pos_seq_freq = pos_seq_freq_chunks(human_article_content)
-    # print("\nHuman POS tag sequences in chunks:")
-    # print(human_pos_seq_freq)
-
-    # ai_pos_seq_freq = pos_seq_freq_chunks(ai_article_content)
-    # print("\nAI POS tag sequences in chunks:")
-    # print(ai_pos_seq_freq)
-
-    # human_pos_seq_freq_sents = pos_seq_freq_sents(human_article_content)
-    # print("\nmost common Human POS tag sequences in sentences:")
-    # print(human_pos_seq_freq_sents)
-
-    # ai_pos_seq_freq_sents = pos_seq_freq_sents(ai_article_content)
-    # print("\nMost common AI POS tag sequences in sentences:")
-    # print(ai_pos_seq_freq_sents)
-
-    # print("\nMost common first 3 POS tags in human sentences")
-    # print(begin_POS_tags(display_sentences(human_article_content)))
-
-    # print("\nMost common first 3 POS tags in AI sentences")
-    # print(begin_POS_tags(display_sentences(ai_article_content)))
-
-    # print("\nMost common last 3 POS tags in human sentences")
-    # print(end_POS_tags(display_sentences(human_article_content)))
-
-    # print("\nMost common last 3 POS tags in AI sentences")
-    # print(end_POS_tags(display_sentences(ai_article_content)))
-
-    # print("\n Sentence lengths of Humans:")
-    # print(sentence_lengths(human_article_content))
-    # print(f"average sentence length: {average_sentence_length(human_article_content)}")
-
-    # print("\n Sentence lengths of AI:")
-    # print(sentence_lengths(ai_article_content))
-    # print(f"average sentence length: {average_sentence_length(ai_article_content)}")
-
-    # print('\nDEP Tag frequency - human content:')
-    # print(dep_tag_frequency(human_article_content))
-
-    # print('\nDEP Tag frequency - ai content:')
-    # print(dep_tag_frequency(ai_article_content))
-
     print("\n Percentages of POS tag frequency in Human texts:")
     percentage_pos_tag_human = (find_percentage(pos_tag_frequency(human_article_content)))
     print(percentage_pos_tag_human)
