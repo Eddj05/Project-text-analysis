901ae7210298b837009757c0701539e70927abdb
KennieSlagter <k.m.slagter@student.rug.nl>
2024-05-31 12:55:08 +0200

    adding more percentages to test with the test set

--- scripts/syntax.py
+++ scripts/syntax.py
@@ -163,65 +163,85 @@ def main():
     human_article_content = (get_content(file_path_human))
     ai_article_content = (get_content(file_path_ai))
 
-    print('POS Tag frequency - human content:')
-    print(pos_tag_frequency(human_article_content))
+    # print('POS Tag frequency - human content:')
+    # print(pos_tag_frequency(human_article_content))
 
-    print('\nPOS Tag frequency - ai content:')
-    print(pos_tag_frequency(ai_article_content))
+    # print('\nPOS Tag frequency - ai content:')
+    # print(pos_tag_frequency(ai_article_content))
 
-    human_pos_seq_freq = pos_seq_freq_chunks(human_article_content)
-    print("\nHuman POS tag sequences in chunks:")
-    print(human_pos_seq_freq)
+    # human_pos_seq_freq = pos_seq_freq_chunks(human_article_content)
+    # print("\nHuman POS tag sequences in chunks:")
+    # print(human_pos_seq_freq)
 
-    ai_pos_seq_freq = pos_seq_freq_chunks(ai_article_content)
-    print("\nAI POS tag sequences in chunks:")
-    print(ai_pos_seq_freq)
+    # ai_pos_seq_freq = pos_seq_freq_chunks(ai_article_content)
+    # print("\nAI POS tag sequences in chunks:")
+    # print(ai_pos_seq_freq)
 
-    human_pos_seq_freq_sents = pos_seq_freq_sents(human_article_content)
-    print("\nmost common Human POS tag sequences in sentences:")
-    print(human_pos_seq_freq_sents)
+    # human_pos_seq_freq_sents = pos_seq_freq_sents(human_article_content)
+    # print("\nmost common Human POS tag sequences in sentences:")
+    # print(human_pos_seq_freq_sents)
 
-    ai_pos_seq_freq_sents = pos_seq_freq_sents(ai_article_content)
-    print("\nMost common AI POS tag sequences in sentences:")
-    print(ai_pos_seq_freq_sents)
+    # ai_pos_seq_freq_sents = pos_seq_freq_sents(ai_article_content)
+    # print("\nMost common AI POS tag sequences in sentences:")
+    # print(ai_pos_seq_freq_sents)
 
-    print("\nMost common first 3 POS tags in human sentences")
-    print(begin_POS_tags(display_sentences(human_article_content)))
+    # print("\nMost common first 3 POS tags in human sentences")
+    # print(begin_POS_tags(display_sentences(human_article_content)))
 
-    print("\nMost common first 3 POS tags in AI sentences")
-    print(begin_POS_tags(display_sentences(ai_article_content)))
+    # print("\nMost common first 3 POS tags in AI sentences")
+    # print(begin_POS_tags(display_sentences(ai_article_content)))
 
-    print("\nMost common last 3 POS tags in human sentences")
-    print(end_POS_tags(display_sentences(human_article_content)))
+    # print("\nMost common last 3 POS tags in human sentences")
+    # print(end_POS_tags(display_sentences(human_article_content)))
 
-    print("\nMost common last 3 POS tags in AI sentences")
-    print(end_POS_tags(display_sentences(ai_article_content)))
+    # print("\nMost common last 3 POS tags in AI sentences")
+    # print(end_POS_tags(display_sentences(ai_article_content)))
 
-    print("\n Sentence lengths of Humans:")
-    print(sentence_lengths(human_article_content))
-    print(f"average sentence length: {average_sentence_length(human_article_content)}")
+    # print("\n Sentence lengths of Humans:")
+    # print(sentence_lengths(human_article_content))
+    # print(f"average sentence length: {average_sentence_length(human_article_content)}")
 
-    print("\n Sentence lengths of AI:")
-    print(sentence_lengths(ai_article_content))
-    print(f"average sentence length: {average_sentence_length(ai_article_content)}")
+    # print("\n Sentence lengths of AI:")
+    # print(sentence_lengths(ai_article_content))
+    # print(f"average sentence length: {average_sentence_length(ai_article_content)}")
 
-    print('\nDEP Tag frequency - human content:')
-    print(dep_tag_frequency(human_article_content))
+    # print('\nDEP Tag frequency - human content:')
+    # print(dep_tag_frequency(human_article_content))
 
-    print('\nDEP Tag frequency - ai content:')
-    print(dep_tag_frequency(ai_article_content))
+    # print('\nDEP Tag frequency - ai content:')
+    # print(dep_tag_frequency(ai_article_content))
 
     print("\n Percentages of POS tag frequency in Human texts:")
-    print(find_percentage(pos_tag_frequency(human_article_content)))
+    percentage_pos_tag_human = (find_percentage(pos_tag_frequency(human_article_content)))
+    print(percentage_pos_tag_human)
 
     print("\n percentages of POS tag frequency in AI texts:")
-    print(find_percentage(pos_tag_frequency(ai_article_content)))
+    percentage_pos_tag_ai = (find_percentage(pos_tag_frequency(ai_article_content)))
+    print(percentage_pos_tag_ai)
 
     print("\n percentages of DEP tag frequency in human texts:")
-    print(find_percentage(dep_tag_frequency(human_article_content)))
+    percentage_dep_freq_human = (find_percentage(dep_tag_frequency(human_article_content)))
+    print(percentage_dep_freq_human)
 
     print("\n percentages of DEP tag frequency in AI texts:")
-    print(find_percentage(dep_tag_frequency(ai_article_content)))
+    percentage_dep_freq_ai = (find_percentage(dep_tag_frequency(ai_article_content)))
+    print(percentage_dep_freq_ai)
+
+    print("\n percentages of the first 3 beginning POS tags in human texts:")
+    percentage_3_pos_beginning_human = find_percentage(begin_POS_tags(display_sentences(human_article_content)))
+    print(percentage_3_pos_beginning_human[0:10])
+
+    print("\n percentages of the first 3 beginning POS tags in human texts:")
+    percentage_3_pos_beginning_ai = find_percentage(begin_POS_tags(display_sentences(ai_article_content)))
+    print(percentage_3_pos_beginning_ai[0:10])
+
+    print("\n percentages of the first 3 beginning POS tags in human texts:")
+    percentage_3_pos_end_human = find_percentage(begin_POS_tags(display_sentences(human_article_content)))
+    print(percentage_3_pos_end_human[0:10])
+    
+    print("\n percentages of the first 3 beginning POS tags in AI texts:")
+    percentage_3_pos_end_ai = find_percentage(begin_POS_tags(display_sentences(human_article_content)))
+    print(percentage_3_pos_end_ai[0:10])
 
 
 if __name__ == "__main__":
