521e577cfd63d10bf8b384fd569b79b5a3b041cf
root <root@DESKTOP-3I9N5MH>
2024-06-01 19:16:35 +0200

    adding documentation to functions and adding comments where neccessary

--- scripts/script.py
+++ scripts/script.py
@@ -3,6 +3,17 @@ from semantic import *
 from pragmatic import *
 
 def test_data(score_list):
+    """
+    Returns if the text is AI-Generated text or Human Generated
+
+    Parameters:
+    score_list (list): a list of integers with scores of each test function
+
+    Returns:
+    label (str): Human Generated or AI-Generated
+
+    Made By: 
+    """
     # initialize a variable which keeps track of the amount of the score of the text
     # this score will decide which label the text will be given
     test_score = sum(score_list)
@@ -19,19 +30,50 @@ def test_data(score_list):
 
 
 def test_average_length(test_text):
+    """
+    Returns a test score given a test text and checking if the
+    average sentence length is more likely to be AI or Human generated
+
+    Parameters:
+    test_text (list): a list containing doc variables (doc = nlp(text))
+
+    Returns:
+    test_score (int): returning a score based on the test text given whether it is more likely to be 
+    AI or Human 
+    
+    Made by:
+    """
     length_test_score = 0
     # get the average sentence length from syntax.py
     average_sent_length = average_sentence_length(test_text)
     # decide whether this applies to human made or ai generated text.
     if average_sent_length > 27.55:
+        # -1 for more likely to be AI
         length_test_score -= 1
     else:
+        # +1 for more likely to be human generated
         length_test_score += 1
 
     return length_test_score
 
 
 def test_pos_tags(test_text, human_article_content, ai_article_content):
+    """
+    Returns a test score based on whether the test text is more likely to be AI or Human generated
+    based on the frequency of POS tags and checking it with average human frequency of POS tags and 
+    average AI frequency of POS tags
+
+    Parameters:
+    test_text (list): a list of doc variable containing the test_text
+    human_article_content (list): list of doc variables containing human texts
+    ai_article_content (list): list of doc variables containing AI generated texts
+
+    Returns:
+    test_score (int): returns a test score whether frequency of pos tags is more likely to be
+    AI or human generated
+
+    Made by: 
+    """
     test_score = 0
     percentage_pos_tag_human = sorted((find_percentage(pos_tag_frequency(human_article_content))))
     percentage_pos_tag_ai = sorted((find_percentage(pos_tag_frequency(ai_article_content))))
@@ -46,6 +88,8 @@ def test_pos_tags(test_text, human_article_content, ai_article_content):
             if test_percentage[0] == ai_percentage[0]:
                 test_percentage_ai = abs(test_percentage[1] - ai_percentage[1])
                 break
+        # check whether the test percentage is closer to the percentage of 
+        # the percentage of human or ai     
         if test_percentage_human < test_percentage_ai:
             test_score += 1
         else:
@@ -55,6 +99,20 @@ def test_pos_tags(test_text, human_article_content, ai_article_content):
 
 
 def test_3_beginning_pos(test_text, human_article_content, ai_article_content):
+    """
+    Returns a test score number based on whether the test text where the 3 beginning POS tags of sentences
+    are more likely to be human or AI.
+
+    Parameters:
+    test_text (list): a list containing doc variable with the test text
+    human_article_content (list): a list containing doc variables containing human generated texts
+    ai_article_content (list): a list of doc variables containing AI generated texts
+
+    Returns:
+    test_score (int): a test score whether it is more likely to be human or AI generated
+
+    Made by: 
+    """
     percentage_3_pos_beginning_human = sorted(find_percentage(begin_POS_tags(display_sentences(human_article_content))))
     percentage_3_pos_beginning_ai = sorted(find_percentage(begin_POS_tags(display_sentences(ai_article_content))))
     percentage_3_pos_beginning_test = sorted(find_percentage(begin_POS_tags(display_sentences(test_text))))
@@ -78,6 +136,22 @@ def test_3_beginning_pos(test_text, human_article_content, ai_article_content):
 
 
 def test_3_end_pos(test_text, human_article_content, ai_article_content):
+    """
+    Returns a test score based on whether the test text is more likely to be human or AI generated
+    based on the frequency of the three ending POS tags of sentences and comparing them with the 
+    frequency of the three ending POS tags of sentences in human and AI generated texts
+
+    Parameters:
+    test_text (list): a list with one doc variable containing the test text
+    human_article_content (list): a list with doc variables containing human generated texts
+    ai_article_content (list): a list with doc variables containing AI generated texts
+
+    Returns:
+    test_score (int): returns an integer telling whether the test text is more likely to be
+    human or AI generated
+
+    Made by:
+    """
     percentage_3_pos_end_human = sorted(find_percentage(end_POS_tags(display_sentences(human_article_content))))
     percentage_3_pos_end_ai = sorted(find_percentage(end_POS_tags(display_sentences(ai_article_content))))
     percentage_3_pos_end_test = sorted(find_percentage(end_POS_tags(display_sentences(test_text))))
@@ -101,6 +175,22 @@ def test_3_end_pos(test_text, human_article_content, ai_article_content):
 
 
 def test_dep_freq(test_text, human_article_content, ai_article_content):
+    """
+    Returns a test score based on whether the test text is more likely to be human or AI generated
+    based on the frequency of dependecy and comparing that with the frequency of dependency in human 
+    and AI generated texts
+
+    Parameters:
+    test_text (list): a list with one doc variable containing the test text
+    human_article_content (list): a list with doc variables containing human generated texts
+    ai_article_content (list): a list with doc variables containing AI generated texts
+
+    Returns:
+    test_score (int): returns an integer telling whether the test text is more likely to be
+    human or AI generated
+
+    Made by:
+    """
     percentage_dep_freq_human = sorted((find_percentage(dep_tag_frequency(human_article_content))))
     percentage_dep_freq_ai = sorted((find_percentage(dep_tag_frequency(ai_article_content))))
     percentage_dep_freq_test = sorted(find_percentage(dep_tag_frequency(test_text)))
@@ -125,6 +215,22 @@ def test_dep_freq(test_text, human_article_content, ai_article_content):
 
 
 def test_asent_polarity(test_text, file_path_human, file_path_ai):
+    """
+    Returns a test score based on whether the test text is more likely to be human or AI generated
+    based on asent polarity of the text and comparing that with the asent polarity of human
+    and AI generated texts
+
+    Parameters:
+    test_text (str): a string containing the test text
+    human_article_content (list): a list with doc variables containing human generated texts
+    ai_article_content (list): a list with doc variables containing AI generated texts
+
+    Returns:
+    test_score (int): returns an integer telling whether the test text is more likely to be
+    human or AI generated
+
+    Made by:
+    """
     human_article_content_asent = get_content_asent(file_path_human)
     ai_article_content_asent = get_content_asent(file_path_ai)
 
@@ -148,6 +254,22 @@ def test_asent_polarity(test_text, file_path_human, file_path_ai):
 
 
 def test_polarity_blob(test_text, human_article_content, ai_article_content):
+    """
+    Returns a test score based on whether the test text is more likely to be human or AI generated
+    based on the blob polarity of the test text and comparing that with the blob polarity of 
+    human and AI generated texts
+
+    Parameters:
+    test_text (list): a list with one doc variable containing the test text
+    human_article_content (list): a list with doc variables containing human generated texts
+    ai_article_content (list): a list with doc variables containing AI generated texts
+
+    Returns:
+    test_score (int): returns an integer telling whether the test text is more likely to be
+    human or AI generated
+
+    Made by:
+    """
     human_polarity_blob = get_blob_polarity(human_article_content)
     ai_polarity_blob = get_blob_polarity(ai_article_content)
     test_text_polarity_blob = get_blob_polarity(test_text)
@@ -162,6 +284,22 @@ def test_polarity_blob(test_text, human_article_content, ai_article_content):
 
 
 def test_subjectivity(test_text, human_article_content, ai_article_content):
+    """
+    Returns a test score based on whether the test text is more likely to be human or AI generated
+    based on the the subjectivity score of the test text and comparing that to human and AI
+    generated texts
+
+    Parameters:
+    test_text (list): a list with one doc variable containing the test text
+    human_article_content (list): a list with doc variables containing human generated texts
+    ai_article_content (list): a list with doc variables containing AI generated texts
+
+    Returns:
+    test_score (int): returns an integer telling whether the test text is more likely to be
+    human or AI generated
+
+    Made by:
+    """
     human_subjectivity = get_subjectivity(human_article_content)
     ai_subjectivity = get_subjectivity(ai_article_content)
     test_text_subjectivity = get_subjectivity(test_text)
@@ -191,13 +329,13 @@ def main():
     ai_article_content = get_content(file_path_ai)
 
     # testing here with the test texts
-    length_test_score = test_average_length(ai_test_text)
-    pos_tag_score = test_pos_tags(ai_test_text, human_article_content, ai_article_content)
-    beginning_3_pos_score = test_3_beginning_pos(ai_test_text, human_article_content, ai_article_content)
-    end_3_pos_score = test_3_end_pos(ai_test_text, human_article_content, ai_article_content)
-    dep_frep_score = test_dep_freq(ai_test_text, human_article_content, ai_article_content)
-    asent_polarity_score = test_asent_polarity(ai_test_text[0].text, file_path_human, file_path_ai)
-    blob_polarity_score = test_polarity_blob(ai_test_text, human_article_content, ai_article_content)
+    length_test_score = test_average_length(human_test_text)
+    pos_tag_score = test_pos_tags(human_test_text, human_article_content, ai_article_content)
+    beginning_3_pos_score = test_3_beginning_pos(human_test_text, human_article_content, ai_article_content)
+    end_3_pos_score = test_3_end_pos(human_test_text, human_article_content, ai_article_content)
+    dep_frep_score = test_dep_freq(human_test_text, human_article_content, ai_article_content)
+    asent_polarity_score = test_asent_polarity(human_test_text[0].text, file_path_human, file_path_ai)
+    blob_polarity_score = test_polarity_blob(human_test_text, human_article_content, ai_article_content)
     subjectivity_score = test_subjectivity(ai_test_text, human_article_content, ai_article_content)
 
     test_list_scores = [length_test_score, pos_tag_score, beginning_3_pos_score, end_3_pos_score, dep_frep_score, asent_polarity_score, blob_polarity_score, subjectivity_score]
